b29fbf107c158982f4eae229756ca1aec31c80b0
Benjamin Petit <benjamin.petit@tide.co>
2019-11-04 22:06:15 +0100

    challenge: show 2 die

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -8,19 +8,25 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage : ImageView
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+    var random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener {
+            rollDice(diceImage1)
+            rollDice(diceImage2)
+        }
     }
 
-    private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
+    private fun rollDice(img: ImageView) {
+        val randomInt = random.nextInt(6) + 1
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -29,6 +35,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
+        img.setImageResource(drawableResource)
     }
 }
